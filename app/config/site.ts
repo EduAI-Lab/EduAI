276d63b5695ca4b98aca82e0459221686b8649cc
Ayyhab <ahabmasudsiddiqui@gmail.com>
2025-12-15 23:23:22 -0800

    Minor change in the name for EduAi Lap , as per Dr.Abdallah's request

--- app/config/site.ts
+++ app/config/site.ts
@@ -1,5 +1,5 @@
 export const projectInfo = {
-  title: "EduAI Map",
+  title: "EduAI Lap",
   subtitle: "Transforming Education with AI",
   description: "An advanced AI-powered learning platform that adapts to individual student needs and enhances educational outcomes.",
   vision: "To provide an accessible platform for the UBC community that leverages generative AI to enhance teaching and learning. Our vision is to establish a sustainable, pedagogically driven framework that learners and educators across all disciplines can rely on to make AI-enhanced education more scalable, secure, accessible, and inclusive.",
