161d6b844bafbc5c353d1ce1792c449dd1147aa2
Ayyhab <ahabmasudsiddiqui@gmail.com>
2026-05-04 19:26:54 -0700

    changed EduAi's name from  Map to Lap

--- app/config/site.ts
+++ app/config/site.ts
@@ -1,5 +1,5 @@
 export const projectInfo = {
-  title: "EduAI Map",
+  title: "EduAI Lap",
   subtitle: "Transforming Education with AI",
   description: "An advanced AI-powered learning platform that adapts to individual student needs and enhances educational outcomes.",
   vision: "To revolutionize education by creating AI-driven learning experiences that are personalized, engaging, and effective for every student.",
@@ -11,12 +11,11 @@ export const siteConfig = {
   navigation: {
     home: "/",
     team: "/team",
-    dashboard: "/dashboard", // change to Login for better wording
+    dashboard: "/dashboard", 
     courses: "/courses",
     chat: "/chat",
   },
 };
-/// remove the line below EduAI Map
 // change the vision to create a : " provide a platform accesisble to UBC community , utilize gen ai and 
 // 
 // our vision is to establish a sustainable pedagogically driven framework that users across discplines can rely on to make ai enhanced teaching and learning more scalable, secure, accessible and inclusive. 
