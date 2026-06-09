111983dc0db34d9fbb67194bac8451cc54ee8a50
yta3216 <yta3216@gmail.com>
2026-06-08 22:06:39 -0700

    fix migration issues

--- apps/core/prisma/migrations/20260604002902_add_unit_admin/migration.sql
+++ apps/core/prisma/migrations/20260604002902_add_unit_admin/migration.sql
@@ -1,8 +1,11 @@
 -- AlterEnum
-ALTER TYPE "UserRole" ADD VALUE 'UNIT_ADMIN';
+DO $$ BEGIN
+  ALTER TYPE "UserRole" ADD VALUE 'UNIT_ADMIN';
+EXCEPTION WHEN duplicate_object THEN NULL;
+END $$;
 
 -- AlterTable
-ALTER TABLE "courses" ADD COLUMN     "department" TEXT;
+ALTER TABLE "courses" ADD COLUMN IF NOT EXISTS "department" TEXT;
 
 -- AlterTable
-ALTER TABLE "user" ADD COLUMN     "authorizedUnits" TEXT[];
+ALTER TABLE "user" ADD COLUMN IF NOT EXISTS "authorizedUnits" TEXT[];
